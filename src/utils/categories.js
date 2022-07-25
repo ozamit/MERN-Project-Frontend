0dae8c62e800aeaf623069edd9197ab54996ccad
ozamit <87121069+ozamit@users.noreply.github.com>
2022-07-26 00:50:07 +0200

    Update categories.js

--- src/utils/categories.js
+++ src/utils/categories.js
@@ -3,5 +3,5 @@ export const categories = [
   { name: "England", id: "62c75754c0f127461d0de73e" },
   { name: "Spain", id: "62c7579ac0f127461d0de740" },
   { name: "Italy", id: "62c757a4c0f127461d0de742" },
-  { name: "Rest Of The World", id: "" },
+  { name: "Rest Of The World", id: "62df1d5ed2a53e6bf2e6d42b" },
 ];
